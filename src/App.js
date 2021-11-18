d7fa1a6e82aeb70ce9131c0f79971a71270790b3
swymerjo <80417034+swymerjo@users.noreply.github.com>
2021-11-18 09:51:37 +0100

    Added axios and created a simple search engine for weather

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import React from "react";
+import Search from "./Search.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Weather App</h1>
+      <Search />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Search.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Search() {
+  const [cityName, setCityName] = useState(null);
+  const [weather, setWeather] = useState({});
+  const [message, setMessage] = useState(null);
+
+  function displayWeather(response) {
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].main,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      image: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "96c6ec35768d7fb6accd0167701b703c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+    setMessage(
+      <div>
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.image} alt="weather-icon" />
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
+  function updateCity(event) {
+    setCityName(event.target.value);
+  }
+
+  return (
+    <div>
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={2000} //3 secs
+      />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          onChange={updateCity}
+        />
+        <input type="submit" />
+      </form>
+      <h2>{message}</h2>
+    </div>
+  );
+}
